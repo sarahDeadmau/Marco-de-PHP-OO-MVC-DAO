d6f5b8f9b4463832e5cc549ff5fb66f276871643
sarahDeadmau <74906557+sarahDeadmau@users.noreply.github.com>
2021-01-22 12:52:33 +0100

    Realizar SPA del form_bienvenida Admin:Sarah
    SPA y mejora del formulario

--- P3_Fase1-Proy_AL/vistas/form_bienvenida.php
+++ P3_Fase1-Proy_AL/vistas/form_bienvenida.php
@@ -5,55 +5,71 @@ include "cabecera.php";
 
     <div>
         <div class='uno'>
-            <label>Nombre del Producto</label>
+            <label>Nombre del producto</label>
             <input type="text" name="nombre" /><br />
-            <label>Descripción</label>
-            <input type="text" name="descripción" /></br>
-            <label>Categoría: </label><br />
-            <input type="checkbox" name="categoria" value="cosmeticos" />Productos Cosméticos<br />
-            <input type="checkbox" name="categoria" value="alimentos" />Alimentos<br />
-            <input type="checkbox" name="categoria" value="productos" />Productos Tecnológicos <br />
 
-            <label>Localizacion</label>
-            <select class="locali" aria-placeholder="Introduzca la localizacion" name="localizacion">
+            <label>Descripción</label>
+            <!--<input type="text" name="descripción" /></br>-->
+            <textarea name="descripción" rows="5" cols="50">Introduzca la descripción del producto: </textarea>
+
+            <label>Categoría: </label><br />
+            <input type="checkbox" name="categoria" value="cosmeticos" />Productos cosméticos<br />
+            <input type="checkbox" name="categoria" value="alimentos" />Alimentos<br />
+            <input type="checkbox" name="categoria" value="productos" />Productos tecnológicos <br />
+
+            <label>Localización</label>
+            <select class="locali" name="localizacion">
                 <option selected>Ubicación del producto</option>
                 <optgroup label="Cosméticos">
-                    <option value="1">A1</option>
-                    <option value="2">A2</option>
-                    <option value="3">A3</option>
+                    <option value="A1">A1</option>
+                    <option value="A2">A2</option>
+                    <option value="A3">A3</option>
                 </optgroup>
                 <optgroup label="Alimentos">
-                    <option value="4">B1</option>
-                    <option value="5">B2</option>
-                    <option value="6">B3</option>
+                    <option value="B1">B1</option>
+                    <option value="B2">B2</option>
+                    <option value="B3">B3</option>
                 </optgroup>
 
                 <optgroup label="Tecnología">
-                    <option value="4">C1</option>
-                    <option value="5">C2</option>
-                    <option value="6">C3</option>
+                    <option value="C1">C1</option>
+                    <option value="C2">C2</option>
+                    <option value="C3">C3</option>
                 </optgroup>
             </select><br />
+
+            <!--<label>Referencia</label>
+            <select name="referencias">
+                <?php
+                /* $referencias = array("H5", "H6", "H7", "H8");
+                foreach ($referencias as $r) {
+                    echo "<option value='$r'>$r</option>";
+                }*/
+                ?> 
+            </select><br />-->
 
             <label>Fecha Creación: </label>
             <input type="date" name="fechaCreacion" /><br />
 
             <label>Stock: </label>
-            <input type="number" name="StocDispo" /><br />
+            <input type="number" name="stockDispo" /><br />
 
-            <label>Codigo Producto: </label>
+            <label>Código del producto: </label>
             <input type="text" name="codProd" /><br />
 
-            <input type="submit" name="Guardar" value="Guardar" />
-            <input type="reset" name="Borrar" value="Borrar" />
+            <input type="submit" name="guardar" value="Guardar" />
+            <!--<input type="submit" name="comprobar" value="Comprobar"> -->
+            <input type="reset" name="borrar" value="Borrar">
 
         </div>
-
-
     </div>
-
 
 </form>
 <?php
+if (isset($resultado)) {
+    echo "<div class='uno'>";
+    echo $resultado;
+    echo "</div>";
+}
 include "pie.php";
 ?>
